32ac209acebc75317e9f155aa633f30eb72e08ba
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 19:07:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,12 +3,12 @@ module Memorable
     def reset_all
       self.all.clear
     end
- 
+
     def count
       self.all.count
     end
   end
- 
+
   module InstanceMethods
     def initialize
       self.class.all << self
