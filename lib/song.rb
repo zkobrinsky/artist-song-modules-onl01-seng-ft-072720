bbe7beb143e4ce9c7aaf89d80ad8dbe07e90cc6f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:49:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,13 +11,13 @@ class Song
     @@all << self
   end
 
-  def self.find_by_name(name)
-    @@all.detect{|a| a.name == name}
-  end
-
-  def self.all
-    @@all
-  end
+  # def self.find_by_name(name)
+  #   @@all.detect{|a| a.name == name}
+  # end
+  # 
+  # def self.all
+  #   @@all
+  # end
 
   # def self.reset_all
   #   self.all.clear
