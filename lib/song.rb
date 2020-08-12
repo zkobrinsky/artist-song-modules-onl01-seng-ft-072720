9bb19bd9e9e53671a209becf873bb7a281944b1f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 19:00:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable
+  include Paramable
 
   @@all = []
 
@@ -11,27 +12,13 @@ class Song
     @@all << self
   end
 
-  # def self.find_by_name(name)
-  #   @@all.detect{|a| a.name == name}
-  # end
-  #
   def self.all
     @@all
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
 
   def artist=(artist)
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
