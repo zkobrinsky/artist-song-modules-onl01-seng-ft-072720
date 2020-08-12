c41c987765cfef322e6b0d237e63b3c3ffc034e4
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 19:13:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
