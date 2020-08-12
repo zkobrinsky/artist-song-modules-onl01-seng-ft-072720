5d6cff6a5ea732454f1f10ad6d15c6f84eeb23ec
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 18:49:29 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,20 @@
 module Memorable
 
+
+  def all
+    @@all
+  end
+
   def reset_all
     all.clear
   end
 
   def count
     all.count
+  end
+
+  def find_by_name(name)
+    @@all.detect{|a| a.name == name}
   end
 
 end
